71b6b25d75b174a6ed9a1fd42e0dd742cb04b793
jinkyuhan <gkswlsrb95@gmail.com>
2024-11-27 04:46:07 +0900

    tmux-yank added

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,5 +1,10 @@
 -- Comment visual regions/lines
 
+local is_tmux = os.getenv 'TMUX' ~= nil
+
+local CTRL_SLASH = is_tmux and '' or '<C-/>'
+local CTRL_QUESTION = is_tmux and '<backspace>' or '<C-?>'
+
 return {
   { -- "gc" to comment visual regions/lines
     'numToStr/Comment.nvim',
@@ -9,18 +14,26 @@ return {
         mode = { 'n', 'v' },
       },
       {
+        '',
+        mode = { 'n', 'v' },
+      },
+      {
         '<C-?>',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<backspace>',
         mode = { 'n', 'v' },
       },
     },
     opts = {
       toggler = {
-        line = '<C-/>',
-        block = '<C-?>',
+        line = CTRL_SLASH,
+        block = CTRL_QUESTION,
       },
       opleader = {
-        line = '<C-/>',
-        block = '<C-?>',
+        line = CTRL_SLASH,
+        block = CTRL_QUESTION,
       },
     },
   },
